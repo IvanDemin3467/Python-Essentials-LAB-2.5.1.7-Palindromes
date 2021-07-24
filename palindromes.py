9824382743ac91a45b90f6fc338f4f5913ad5b88
IvanDemin3467 <85740662+IvanDemin3467@users.noreply.github.com>
2021-07-24 08:56:43 +0600

    Added check for blank strings on input

--- palindromes.py
+++ palindromes.py
@@ -3,11 +3,12 @@
 def user_input():
     # This function prompts the user and validates the input
     # It returns text in lower case without spaces
-    # Now check user input for alphanumeric characters and spaces
+    # Now check user input for alphanumeric characters and spaces.
+    # Blank strings are not allowed
     while True:
         text = input("Enter one line of text to check (Roman alphabet only): ")
         text = text.replace(" ", "") # spaces are allowed
-        if not text.isalpha():
+        if not text.isalpha() or len(text) == 0:
             print("Invalid input. Only alphabetic characters and spaces are allowed.")
         else:
             return text
@@ -17,17 +18,18 @@ def is_palindrome(text):
     # Main algorithm that accepts string and returns True if string is a palindrome.
     # Palindrome is a word which look the same when read forward and backward.
     # For example, "kayak" is a palindrome, while "loyal" is not.
-    text = text.lower()            # we will work only with lower case letters
-    text = text.replace(" ", "")   # spaces are not taken into account 
-    chars = list(text)             # convert string to list named 'chars'
-    inverted = chars[:]            # copy list to new list named 'inverted' ...
-    inverted.reverse()             # ... and invert it
-    is_pal = True                  # let's assume that text is a palindrome
-    for i in range(len(chars)):    # iterate over characters in text
-        if chars[i] != inverted[i]:# compare normal and inverted texts
-            is_pal = False         # if current characters do not match -> not a palindrome
-            break                  # terminate loop
-    return is_pal                  # return our judgment
+    if len(text) == 0: return False # blank strings are not allowed
+    text = text.lower()             # we will work only with lower case letters
+    text = text.replace(" ", "")    # spaces are not taken into account 
+    chars = list(text)              # convert string to list named 'chars'
+    inverted = chars[:]             # copy list to new list named 'inverted' ...
+    inverted.reverse()              # ... and invert it
+    is_pal = True                   # let's assume that text is a palindrome
+    for i in range(len(chars)):     # iterate over characters in text
+        if chars[i] != inverted[i]: # compare normal and inverted texts
+            is_pal = False          # if current characters do not match -> not a palindrome
+            break                   # terminate loop
+    return is_pal                   # return our judgment
 
 
 def tests():
